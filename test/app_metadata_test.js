0e16abdac48b70bd2dbdd05a1056cdfb5ad9212f
exp-dev-dj <zji@expedia.com>
2021-07-13 09:49:35 -0700

    update test

--- test/app_metadata_test.js
+++ test/app_metadata_test.js
@@ -6,26 +6,18 @@ const expect = chai.expect;
 
 describe('App Metadata Test', () => {
   it ('Question 1 should return apps grouped by team', async () => {
-    try {
-      const appsByTeam = await q1.getAllApplicationNamesGroupedByTeam();
-      expect(appsByTeam.Developer_Experience.length).to.equal(4);
-      expect(appsByTeam.Delivery_Platform.length).to.equal(6);
-      expect(appsByTeam.Developer_Experience.indexOf('app_0')).to.not.equal(-1);
-      expect(appsByTeam.Delivery_Platform.indexOf('app_0')).to.equal(-1);
-    } catch (e) {
-      console.error(e);
-    }
+    const appsByTeam = await q1.getAllApplicationNamesGroupedByTeam();
+    expect(appsByTeam.Developer_Experience.length).to.equal(4);
+    expect(appsByTeam.Delivery_Platform.length).to.equal(6);
+    expect(appsByTeam.Developer_Experience.indexOf('app_0')).to.not.equal(-1);
+    expect(appsByTeam.Delivery_Platform.indexOf('app_0')).to.equal(-1);
   });
 
   it('Question 2 should return appsByTeam with results from paginated API', async () => {
-    try {
-      const appsByTeam = await q2.getAllApplicationNamesGroupedByTeamWithPagination();
-      expect(appsByTeam.Developer_Experience.length).to.equal(68);
-      expect(appsByTeam.Delivery_Platform.length).to.equal(135);
-      expect(appsByTeam.Developer_Experience.indexOf('app_0')).to.not.equal(-1);
-      expect(appsByTeam.Delivery_Platform.indexOf('app_0')).to.equal(-1);
-    } catch (e) {
-      console.error(e);
-    }
+    const appsByTeam = await q2.getAllApplicationNamesGroupedByTeamWithPagination();
+    expect(appsByTeam.Developer_Experience.length).to.equal(68);
+    expect(appsByTeam.Delivery_Platform.length).to.equal(135);
+    expect(appsByTeam.Developer_Experience.indexOf('app_0')).to.not.equal(-1);
+    expect(appsByTeam.Delivery_Platform.indexOf('app_0')).to.equal(-1);
   });
 });
